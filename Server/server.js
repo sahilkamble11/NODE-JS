da4c713206608afabd1861d55ba791534c025d4c
Sahil161967 <sahilbajkamble@gmail.com>
2025-12-10 12:22:10 +0530

    Set Content type as HTML of response

--- Server/server.js
+++ Server/server.js
@@ -2,7 +2,8 @@ var http=require('http');
 var onRequestHandler=function(request,response){
     //console.log("Request Received...");
 
-    response.write("Response from server :)");
+    response.writeHead(200,{'Content-Type':'text/html'});
+    response.write("<h1>Response from server :)</h1>");
     response.end();
 }
 var server=http.createServer(onRequestHandler);
