046b61981bdec4bac28792ecba4f486ae2f3240c
Sahil161967 <sahilbajkamble@gmail.com>
2025-12-10 12:44:18 +0530

    Added Empty file condition

--- FileIOApp/test.js
+++ FileIOApp/test.js
@@ -3,8 +3,14 @@ var fileName="./data/result.txt";
 
 
 var onFileRead=function(err,data){
-    console.log("Data Available in File");
-    console.log(data.toString());
+    
+    if(err){
+        console.log("Data not Available");
+    }
+    else{
+        console.log("Data Available in File");
+        console.log(data.toString());
+    }
     
 }
 fs.readFile(fileName,onFileRead);
