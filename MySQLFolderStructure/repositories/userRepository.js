58261cd732b83043d11ad970643b4a2ab70f0a64
Sahil161967 <sahilbajkamble@gmail.com>
2026-01-04 12:02:57 +0530

    Replaced Stored Procedures with DB Queries

--- MySQLFolderStructure/repositories/userRepository.js
+++ MySQLFolderStructure/repositories/userRepository.js
@@ -1,21 +1,28 @@
 const connection = require('../config/db');
 
 exports.insertUser = (name, contact, result) => {
-    const sql = "INSERT INTO users(name,contact) VALUES(?,?)";
+    const sql = "call spinsertUser(?,?)";
     connection.query(sql, [name, contact], result);
 };
 
 exports.deleteUser = (id, result) => {
-    const sql = "DELETE FROM users WHERE id=?";
+    const sql = "call spdeleteUser(?)";
     connection.query(sql, [id], result);
 };
 
 exports.getUsers = (result) => {
-    const sql = "SELECT * FROM users";
-    connection.query(sql, result);
+     
+    connection.query('CALL spgetUsers()',(err, res)=>{
+        if(err){
+            result(err,null);
+        }
+        else{
+            result(null,res[0]);
+        }
+    });
 };
 
 exports.updateUser = (id, name, contact, result) => {
-    const sql = "UPDATE users SET name=?, contact=? WHERE id=?";
-    connection.query(sql, [name, contact, id], result);
+    const sql = "call spupdateUsers(?,?,?)";
+    connection.query(sql, [id,name, contact], result);
 };
